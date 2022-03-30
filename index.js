0ac082c08ab89b27facf1b16d053f73334ce7b11
filonushka <lero4ka.filonova@mail.ru>
2022-03-31 01:53:34 +0300

    feat: add burger-menu

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const burgerMenu = document.querySelector('.burger');
+const navMenu = document.querySelector('.nav');
+
 if (burgerMenu) {
-    const navMenu = document.querySelector('.nav');
     burgerMenu.addEventListener('click', function(e){
         document.body.classList.toggle('lock');
         burgerMenu.classList.toggle('active');
         navMenu.classList.toggle('active')
     })
 }
+
+// if (navMenu) {(burgerMenu.classList.contains('active'), function(e){
+//     document.body.classList.remove('lock');
+//     burgerMenu.classList.remove('active');
+//     navMenu.classList.remove('active')
+// }
+// )}
